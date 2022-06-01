2f311c6b1af2ef05716df634dff3a34abfea7fcb
blueray17 <58415521+blueray17@users.noreply.github.com>
2022-06-01 09:19:54 +0700

    Add files via upload

---
+++ wifi_mic.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 08:42:15 2022
+
+@author: ipin
+"""
+
+import pyaudio
+import socket
+
+UDP_IP = "192.168.214.232"
+UDP_PORT = 1234
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=32, channels=1, rate=11111, output=True)
+
+try:
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer de 1024 bytes
+        stream.write(data)
+
+except KeyboardInterrupt:  #precionar Crtl + C para salir
+    print("Cerrando...")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
